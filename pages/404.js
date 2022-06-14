6c95c37fc813b836ddfcd8511a03585a51937b87
jchen-vercel <jason.chen@vercel.com>
2022-06-14 14:01:04 -0400

    final commit

---
+++ pages/404.js
@@ -0,0 +1,4 @@
+function PageNotFound() {
+  return <h1> Fancy 404 page text</h1>;
+}
+export default PageNotFound;
---
+++ pages/product/index.js
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+function ProductList({ productId = 100 }) {
+  return (
+    <>
+      <h2>
+        <Link href="/product/1">
+          <a>Product 1</a>
+        </Link>
+      </h2>
+      <h2>
+        {" "}
+        <Link href="/product/2">
+          <a>Product 2</a>
+        </Link>
+      </h2>
+      <h2>
+        {" "}
+        <Link href="/product/3" replace>
+          <a>Product 3</a>
+        </Link>
+        <Link href={`/product/${productId}`}>
+          <a>Product {`${productId}`}</a>
+        </Link>
+      </h2>
+      <Link href="/">
+        <a>Back to Home</a>
+      </Link>
+    </>
+  );
+}
+
+export default ProductList;
